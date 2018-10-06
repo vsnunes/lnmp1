13ff664f2da97390e6e4b710bf22664cd9ae5663
Vítor Nunes <vitor.sobrinho.nunes@sapo.pt>
2018-10-06 18:00:06 +0100

    Add generic functions

--- a/run.sh
+++ a/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-#Symbol table location
-SYMBOL_FILE=../syms.txt
+source ../generic.sh
 
 ################### simbolos ################
 #
 # Compila e gera a versão gráfica do transdutor mmm2mm exercicio 1a)
-fstcompile --isymbols=$SYMBOL_FILE --osymbols=$SYMBOL_FILE  mmm2mm.txt | fstarcsort > mmm2mm.fst
-fstdraw    --isymbols=$SYMBOL_FILE --osymbols=$SYMBOL_FILE --portrait mmm2mm.fst | dot -Tpdf  > mmm2mm.pdf
+compile mmm2mm
+draw    mmm2mm
+
---
+++ generic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This file contains a simplification of function calls to make invocations more clear
+
+#Symbol table location
+SYMBOL_FILE=../syms.txt
+
+#Compile a FST
+# Arg1: the name of the file without extension
+function compile {
+        fstcompile --isymbols=$SYMBOL_FILE --osymbols=$SYMBOL_FILE  $1.txt | fstarcsort > $1.fst
+}
+
+#Draw in PDF the FST
+# Arg1: the name of the file without extension
+function draw {
+        fstdraw    --isymbols=$SYMBOL_FILE --osymbols=$SYMBOL_FILE --portrait $1.fst | dot -Tpdf  > $1.pdf
+}
+
