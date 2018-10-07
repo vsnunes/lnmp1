7f2630cfca38b6d734647be8d61808831c8509b5
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2018-10-07 13:24:00 +0100

    adds misto2numerico

--- a/run.sh
+++ a/run.sh
@@ -3,17 +3,28 @@ source ../generic.sh
 
 ################### simbolos ################
 #
-# Compila e gera a versão gráfica do transdutor mmm2mm exercicio 1a)
+# Compila e gera a versão gráfica do transdutor mmm2mm e misto2numerico exercicio 1a)
+compile dd
 compile mmm2mm
-draw    mmm2mm
+compile aaaa
+draw mmm2mm
+fstconcat dd.fst mmm2mm.fst > ddmmm2mm.fst
+fstconcat ddmmm2mm.fst aaaa.fst > misto2numerico.fst
+draw misto2numerico
 
 # Testes
 echo "** Testes:"
 
 echo -n "testeJAN: "
 compile testeJAN
+compile testeMISTO
+
 #fstrmepsilon testeJAN.fst | fsttopsort | fstprint --isymbols=$SYMBOL_FILE
 fstcompose testeJAN.fst mmm2mm.fst  | fstarcsort > composeJAN.fst
+fstcompose testeMISTO.fst misto2numerico.fst | fstarcsort > compose25122018.fst
 echo -n "O formato decimal de JAN é: "
 show composeJAN
+echo " "
+echo -n "O formato numeral de 25/DEZ/2018 é: "
+show compose25122018
 echo " "
