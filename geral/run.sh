2c4351822e9944ea0a0814d64cd95528a17b24bb
Vitor Nunes <vitor.sobrinho.nunes@sapo.pt>
2018-10-09 16:16:31 +0100

    adds e

--- geral/run.sh
+++ geral/run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 source ../generic.sh
 
-#Compilacao dos transdutores base
-compile barra
-draw barra
-
-
 #alinea a. 
 #mmm2mm
 compile mmm2mm
@@ -59,6 +54,7 @@ compile nn
 draw nn
 # O FST 1_29 já contem o extenso de números entre 1 e 29, assim reaproveita-se isso e junta-se o restante
 fstunion 1_29nn.fst nn.fst > 1_99nn.fst
+draw 1_99nn
 # Seculo FST contem o necessário para traduzir os primeiros dois digitos do ano: 20xx -> dois mil e ...
 compile seculo
 draw seculo
@@ -98,3 +94,64 @@ fstconcat diamesdata2texto.fst barra.fst > diamesbarradata2texto.fst
 draw diamesbarradata2texto
 fstconcat diamesbarradata2texto.fst ano.fst > data2texto.fst 
 draw data2texto
+
+#alinea e
+#83567
+compile 83567_numerico
+draw 83567_numerico
+compile 83567_misto
+draw 83567_misto
+compile 83567_pt
+draw 83567_pt
+fstcompose 83567_numerico.fst numerico2texto.fst > 83567_numerico2texto.fst
+draw 83567_numerico2texto
+show 83567_numerico2texto
+echo""
+fstcompose 83567_numerico.fst data2texto.fst > 83567_data2texto.fst
+draw 83567_data2texto
+show 83567_data2texto
+echo""
+fstcompose 83567_misto.fst misto2numerico.fst > 83567_misto2numerico.fst
+draw 83567_misto2numerico
+show 83567_misto2numerico
+echo""
+fstcompose 83567_misto.fst misto2texto.fst > 83567_misto2texto.fst
+draw 83567_misto2texto
+show 83567_misto2texto
+echo""
+fstcompose 83567_pt.fst pt2en.fst > 83567_pt2en.fst
+draw 83567_pt2en
+show 83567_pt2en
+echo""
+
+
+#83576
+compile 83576_numerico
+draw 83576_numerico
+compile 83576_misto
+draw 83576_misto
+compile 83576_pt
+draw 83576_pt
+fstcompose 83576_numerico.fst numerico2texto.fst > 83576_numerico2texto.fst
+draw 83576_numerico2texto
+show 83576_numerico2texto
+echo""
+fstcompose 83576_numerico.fst data2texto.fst > 83576_data2texto.fst
+draw 83576_data2texto
+show 83576_data2texto
+echo""
+fstcompose 83576_misto.fst misto2numerico.fst > 83576_misto2numerico.fst
+draw 83576_misto2numerico
+show 83576_misto2numerico
+echo""
+fstcompose 83576_misto.fst misto2texto.fst > 83576_misto2texto.fst
+draw 83576_misto2texto
+show 83576_misto2texto
+echo""
+fstcompose 83576_pt.fst pt2en.fst > 83576_pt2en.fst
+draw 83576_pt2en
+show 83576_pt2en
+echo""
+
+
+
